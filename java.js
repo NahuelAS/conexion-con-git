cbd9a398f11b7e5731790b63fffd49217bb3b229
Nahuel Stella <nahuelprg@gmail.com>
2023-01-24 19:42:08 -0300

    mensaje otra vez

---
+++ java.js
@@ -0,0 +1,14 @@
+const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const p = document.querySelector('#result');
+
+form.addEventListener('submit', sumarInputValues); //primero va el elvento que se quiera escuchar y despues cual funcion tiene que llamar cuando ocurra el evento. 
+
+function sumarInputValues(event) {
+    event.preventDefault();
+   const sumaInputs = input1.value + input2.value;
+   p.innerText = "Resultado:" + sumaInputs;
+}
